8450178d49c5a6b9fc783b14f280ee3c5962cca3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-10-31 14:17:19 +1300

    Initial Commit

---
+++ DictionarySort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DictionarySort
+//
+//  Created by Samuel Williams on 31/10/11.
+//  Copyright (c) 2011 Orion Transfer Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
