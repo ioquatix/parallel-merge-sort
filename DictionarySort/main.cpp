13147347af47cb680838f5266c4fcc0fb6cb997a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-11-01 22:42:03 +1300

    Improved performance.

--- DictionarySort/main.cpp
+++ DictionarySort/main.cpp
@@ -91,105 +91,145 @@ struct pointer_less_than
 // A parallel merge sort algorithm template implemented using C++0x11 threads.
 namespace ParallelMergeSort {
     template <typename ArrayT, typename ComparatorT>
-    void partition(bool parity, ArrayT & array, ArrayT & temporary, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound, std::size_t threaded);
+    void partition(ArrayT & array, ArrayT & temporary, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound, std::size_t threaded);
     
     template <typename ArrayT, typename ComparatorT>
     void merge (ArrayT & array, ArrayT & merged, ComparatorT & comparator, std::size_t lowerBound, std::size_t middleBound, std::size_t upperBound);
     
     template <typename ArrayT, typename ComparatorT>
     struct ParallelPartition {
-        bool parity;
-        
         ArrayT & array, & temporary;
         ComparatorT & comparator;
         std::size_t lowerBound, upperBound, threaded;
         
         void operator()() {
-            partition(parity, array, temporary, comparator, lowerBound, upperBound, threaded);
+            partition(array, temporary, comparator, lowerBound, upperBound, threaded);
         }
     };
     
-    // Provide an array, and an upper and lower bound, along with the number of threads to use.
-    // If parity is true, the data is in array,
-    // If parity is false, the data is in temporary.
+    // Sequential partition algorithm. Provide an array, and an upper and lower bound to sort.
     template <typename ArrayT, typename ComparatorT>
-    void partition(bool parity, ArrayT & array, ArrayT & temporary, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound) {
+    void partition(ArrayT & source, ArrayT & destination, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound) {
         // At this point we are trying to optimise the linear performance,
         // so we unfold the bottom cases to avoid about half of the recursion.
-        
         std::size_t count = upperBound - lowerBound;
         
-        if (count == 1) {
-            if (parity) {
-                temporary[lowerBound] = array[lowerBound];
+        if (count == 0) {
+            // Nothing to do here =(
+            return;
+        } else if (count == 1) {
+            destination[lowerBound] = source[lowerBound];
+        } else if (count == 2) {
+            destination[lowerBound] = source[lowerBound];
+            destination[lowerBound + 1] = source[lowerBound + 1];
+            
+            if (comparator(source[lowerBound], source[lowerBound+1])) {
+                destination[lowerBound] = source[lowerBound];
+                destination[lowerBound + 1] = source[lowerBound + 1];
             } else {
-                array[lowerBound] = temporary[lowerBound];
+                destination[lowerBound] = source[lowerBound + 1];
+                destination[lowerBound + 1] = source[lowerBound];
             }
         } else if (count > 1) {
             std::size_t middle = (lowerBound + upperBound) / 2;
             
-            partition(!parity, array, temporary, comparator, lowerBound, middle);
-            partition(!parity, array, temporary, comparator, middle, upperBound);
+            partition(destination, source, comparator, lowerBound, middle);
+            partition(destination, source, comparator, middle, upperBound);
             
-            if (parity) {
-                merge(array, temporary, comparator, lowerBound, middle, upperBound);
-            } else {
-                merge(temporary, array, comparator, lowerBound, middle, upperBound);
-            }
+            merge(source, destination, comparator, lowerBound, middle, upperBound);
         }
-        
-        // If we get here, we are at the bottom of the tree.
-        // By default, data is in both array and temporary.
     }
-
-    const bool USE_THREADS = true;
+    
+    template <typename ArrayT, typename ComparatorT, int DIRECTION>
+    struct ParallelMerge {
+        ArrayT & array, & merged;
+        ComparatorT & comparator;
+        std::size_t start, finish;
+        
+        void operator()() {
+            std::size_t outer = start;
+            std::size_t inner = finish;
+            std::size_t offset = start;
+                        
+            while (offset != finish) {
+                // This will be true if the items are ordered correctly.
+                bool comparison;
+                
+                // If two items are the same, we give priority to the outer most partition.
+                // Since this is a constant, we assume the compiler will optimize this block of code.
+                if (DIRECTION == 1) {
+                    comparison = comparator(array[inner], array[outer]);
+                } else if (DIRECTION == -1) {
+                    comparison = comparator(array[outer], array[inner]);
+                }
+                
+                if (comparison) {
+                    merged[offset] = array[inner];
+                    offset += DIRECTION;
+                    inner += DIRECTION;
+                } else {
+                    merged[offset] = array[outer];
+                    offset += DIRECTION;
+                    outer += DIRECTION;
+                }
+            }            
+        }
+    };
+    
+    // Use this to control whether parallal partition is used.
+    const bool PARALLEL_PARTITION = true;
+    
+    // Use this to control whether parallel merge is used.
+    const bool PARALLEL_MERGE = true;
+    
+    // If you make this number too small, e.g. <= 2, you may cause synchronsation issues, because you will force parallelisation
+    // for base cases which actually need to be sequential to ensure that comparison cache is generated correctly.
+    const std::size_t PARALLEL_MERGE_MINIMUM_COUNT = 128;
     
     // Provide an array, and an upper and lower bound, along with the number of threads to use.
     // If parity is true, the data is in array,
     // If parity is false, the data is in temporary.
     template <typename ArrayT, typename ComparatorT>
-    void partition(bool parity, ArrayT & array, ArrayT & temporary, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound, std::size_t threaded) {
+    void partition(ArrayT & source, ArrayT & destination, ComparatorT & comparator, std::size_t lowerBound, std::size_t upperBound, std::size_t threaded) {
+        std::size_t count = upperBound - lowerBound;
+
         //std::cerr << "Partition: " << lowerBound << " -> " << upperBound << std::endl;
-        if (lowerBound < (upperBound - 1)) {
+        if (count > 1) {
             std::size_t middle = (lowerBound + upperBound) / 2;
             
-            if (threaded > 0) {
-                //Benchmark::Timer tp;
+            if (PARALLEL_PARTITION && threaded > 0) {
+                // We could check whether there is any work to do before creating threads, but we assume
+                // that threads will only be created high up in the tree by default, so there *should*
+                // be a significant work available per-thread.
+                ParallelPartition<ArrayT, ComparatorT> 
+                    lowerPartition = {destination, source, comparator, lowerBound, middle, threaded - 1}, 
+                    upperPartition = {destination, source, comparator, middle, upperBound, threaded - 1};
                 
-                if (USE_THREADS) {
-                    // We could check whether there is any work to do before creating threads, but we assume
-                    // that threads will only be created high up in the tree by default, so there *should*
-                    // be a significant work available per-thread.
-                    ParallelPartition<ArrayT, ComparatorT> 
-                        lowerPartition = {!parity, array, temporary, comparator, lowerBound, middle, threaded - 1}, 
-                        upperPartition = {!parity, array, temporary, comparator, middle, upperBound, threaded - 1};
-                    
-                    std::thread 
-                        lowerThread(lowerPartition),
-                        upperThread(upperPartition);
-                    
-                    upperThread.join();
-                    lowerThread.join();
-                } else {
-                    partition(!parity, array, temporary, comparator, lowerBound, middle, threaded - 1);
-                    partition(!parity, array, temporary, comparator, middle, upperBound, threaded - 1);
-                }
+                std::thread 
+                    lowerThread(lowerPartition),
+                    upperThread(upperPartition);
                 
-                //std::cerr << "Partition time: " << tp.time() << std::endl;
+                upperThread.join();
+                lowerThread.join();                
             } else {
-                partition(!parity, array, temporary, comparator, lowerBound, middle);
-                partition(!parity, array, temporary, comparator, middle, upperBound);
+                partition(destination, source, comparator, lowerBound, middle);
+                partition(destination, source, comparator, middle, upperBound);
             }
             
-            //Benchmark::Timer tm;
-            
-            if (parity) {
-                merge(array, temporary, comparator, lowerBound, middle, upperBound);
+            if (PARALLEL_MERGE && threaded > 0 && count > PARALLEL_MERGE_MINIMUM_COUNT) {
+                // By the time we get here, we are sure that both left and right partitions have been merged
+                ParallelMerge<ArrayT, ComparatorT, +1> leftMerge = {source, destination, comparator, lowerBound, middle};
+                ParallelMerge<ArrayT, ComparatorT, -1> rightMerge = {source, destination, comparator, upperBound-1, middle-1};
+                
+                std::thread
+                    leftThread(leftMerge),
+                    rightThread(rightMerge);
+                
+                leftThread.join();
+                rightThread.join();
             } else {
-                merge(temporary, array, comparator, lowerBound, middle, upperBound);
+                merge(source, destination, comparator, lowerBound, middle, upperBound);
             }
-            
-            //std::cerr << "Merge time: " << tm.time() << std::endl;
         }
     }
     
@@ -203,21 +243,26 @@ namespace ParallelMergeSort {
         std::size_t right = middleBound;
         std::size_t offset = lowerBound;
         
+        // We merge both sub-sequences, defined as [lowerBound, middleBound] and [middleBound, upperBound].
         while (true) {
-            if (left == middleBound) {
-                // We have no more elements in lower half.
-                std::copy(array.begin() + right, array.begin() + upperBound, merged.begin() + offset);
-                break;
-            } else if (right == upperBound) {
-                // We have no more elements in upper half.
-                std::copy(array.begin() + left, array.begin() + middleBound, merged.begin() + offset);
-                break;
-            }
-            
             if (comparator(array[left], array[right])) {
                 merged[offset++] = array[left++];
+                
+                // If we have adjusted left, we may have exhausted left side:
+                if (left == middleBound) {
+                    // We have no more elements in lower half.
+                    std::copy(array.begin() + right, array.begin() + upperBound, merged.begin() + offset);
+                    break;
+                }
             } else {
                 merged[offset++] = array[right++];
+                
+                // As above, we may have exhausted right side:
+                if (right == upperBound) {
+                    // We have no more elements in upper half.
+                    std::copy(array.begin() + left, array.begin() + middleBound, merged.begin() + offset);
+                    break;
+                }
             }
         }
     }
@@ -225,23 +270,22 @@ namespace ParallelMergeSort {
     template <typename ArrayT, typename ComparatorT>
     void sort(ArrayT & array, ComparatorT & comparator, std::size_t threaded = 2) {
         ArrayT temporary(array.begin(), array.end());
-        //temporary.resize(array.size());
         
         if (threaded == 0)
-            partition(false, array, temporary, comparator, 0, array.size());
+            partition(temporary, array, comparator, 0, array.size());
         else
-            partition(false, array, temporary, comparator, 0, array.size(), threaded);
+            partition(temporary, array, comparator, 0, array.size(), threaded);
     }
 }
 
 namespace DictionarySort {
-    template <typename CharT>
+    typedef std::uint64_t IndexT;
+    
+    template <typename CharT, typename MapT>
     class Dictionary {    
     public:
         typedef std::vector<CharT> WordT;
-        typedef std::vector<WordT> WordsT;
-        
-        typedef std::uint64_t IndexT;
+        typedef std::vector<WordT> WordsT;        
         typedef std::vector<IndexT> OrderT;
         
         static const int ORDERED_LT = -1;
@@ -257,14 +301,14 @@ namespace DictionarySort {
         {
             std::size_t offset = 0;
             
-            do {
+            while (offset < lhs.size() && offset < rhs.size()) {
                 if (lhs[offset] < rhs[offset])
                     return ORDERED_LT;
                 else if (lhs[offset] > rhs[offset])
                     return ORDERED_GT;
                 
                 offset += 1;
-            } while (offset < lhs.size() && offset < rhs.size());
+            }
             
             if (lhs.size() == rhs.size())
                 return ORDERED_EQ;
@@ -278,7 +322,11 @@ namespace DictionarySort {
         
     private:
         WordT m_alphabet;
-        std::map<CharT, IndexT> m_characterOrder;
+        
+        MapT m_characterOrder;
+        //int m_characterOrder[256];
+        //std::map<CharT, IndexT> m_characterOrder;
+        
         IndexT width;
         IndexT charactersPerSegment;
         
@@ -287,8 +335,11 @@ namespace DictionarySort {
             Dictionary * dictionary;
             mutable OrderT order;
             
+            // The first time this function is called, it must be guaranteed from a single thread.
+            // After that, it can be called from multiple threads at the same time.
+            // The parallel merge sort algorithm guarantees this.
             OrderT & fetchOrder() const {
-                if (order.size() == 0)
+                if (order.size() == 0 && word.size() > 0)
                     order = dictionary->sum(word);
                 
                 return order;
@@ -362,7 +413,7 @@ namespace DictionarySort {
                 orderedWord->dictionary = this;
                 
                 // We can generate this as part of the sorting process. Because the sort is parallel,
-                // generation of word order is distributed across multiple processors.
+                // generation of word order (which is relatively expensive) is distributed across multiple processors.
                 //orderedWord->order = sum(*i);
                 
                 words.push_back(orderedWord);
@@ -374,7 +425,7 @@ namespace DictionarySort {
             // Sort the words using built-in sorting algorithm.
             pointer_less_than<OrderedWord> comparator;
             //std::sort(words.begin(), words.end(), comparator);
-            ParallelMergeSort::sort(words, comparator, 1);
+            ParallelMergeSort::sort(words, comparator, 3);
             std::cerr << "Sort time: " << ts.time() << std::endl;
             
             // Copy sorted words to output vector.
@@ -386,16 +437,35 @@ namespace DictionarySort {
     };
 }
 
-int main (int argc, const char * argv[])
-{    
-    typedef DictionarySort::Dictionary<char> ASCIIDictionaryT;
+void testSort ()
+{
+    const int data[] = {6, 9, 5, 4, 8, 3, 2, 1, 7};
+    std::vector<int> v(data, data+(sizeof(data)/sizeof(int)));
+    
+    std::cerr << "Sorting " << v << std::endl;
+
+    std::less<int> comparator;
+    ParallelMergeSort::sort(v, comparator, 1);
+    
+    std::cerr << "Sorted  " << v << std::endl;   
+}
+
+void testDictionary ()
+{
+    // This defines a dictionary based on ASCII characters.
+    typedef DictionarySort::Dictionary<char, DictionarySort::IndexT[256]> ASCIIDictionaryT;
+    
+    // For unicode characters, you could use something like this:
+    // typedef DictionarySort::Dictionary<uint32_t, std::map<uint32_t, DictionarySort::IndexT>> UCS32DictionaryT;
+    // Be aware that 
+    
     std::string s = "abcdefghijklmnopqrstuvwxyz";
-    ASCIIDictionaryT::WordT alphabet(s.begin(), s.end()), w1, w2, w3;
+    ASCIIDictionaryT::WordT alphabet(s.begin(), s.end());
     
     ASCIIDictionaryT dictionary(alphabet);
     
     ASCIIDictionaryT::WordsT words, sortedWords;
-    std::ifstream inputFile("words.txt");
+    std::ifstream inputFile("/usr/share/dict/words");
     while (inputFile.good()) {
         std::string inputData;
         inputFile >> inputData;
@@ -420,8 +490,16 @@ int main (int argc, const char * argv[])
     
     std::cerr << "Time: " << (elapsedTime / K) << std::endl;
     //std::cerr << "Sorted: " << sortedWords << std::endl;
-
+        
     std::cerr << "Finished." << std::endl;
+}
+
+int main (int argc, const char * argv[])
+{   
+    //testSort();
+    
+    testDictionary();
+    
     return 0;
 }
 
