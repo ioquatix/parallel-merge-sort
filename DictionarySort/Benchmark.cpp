9f47ec6fd1bdc0cf3f204edeaa95fb5a57776d2f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-26 03:31:26 +1300

    Fixed an error in the rename of system_time.

--- DictionarySort/Benchmark.cpp
+++ DictionarySort/Benchmark.cpp
@@ -12,7 +12,7 @@
 
 // A timer class for quickly checking the wall-clock performance of code.
 namespace Benchmark {
-    TimeT syste_time () {
+    static TimeT system_time () {
         static struct timeval t;
         gettimeofday (&t, (struct timezone*)0);
         return ((TimeT)t.tv_sec) + ((TimeT)t.tv_usec / 1000000.0);
@@ -27,12 +27,12 @@ namespace Benchmark {
     }
     
     void Timer::reset () {
-        this->_last = syste_time();
+        this->_last = system_time();
         this->_total = 0.0;
     }
     
     TimeT Timer::time () const {
-        TimeT current = syste_time();
+        TimeT current = system_time();
         this->_total += current - this->_last;
         this->_last = current;
         return this->_total;
